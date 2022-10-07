eb41d07fdadd93f1f835f9834c63ccf0feb3918a
Akiyoshi Suda <aki.suda@protonmail.com>
2022-10-08 02:37:04 +0900

    Remove unused services

--- users/services/default.nix
+++ users/services/default.nix
@@ -2,12 +2,4 @@
 
 {
   services.gnome-keyring.enable = true;
-
-  # enable udisks2 system-wide for this
-  services.udiskie.enable = true;
-
-  # Note, for the applet to work, the 'blueman' service should be enabled system-wide.
-  services.blueman-applet.enable = true;
-
-  services.network-manager-applet.enable = true;
 }
