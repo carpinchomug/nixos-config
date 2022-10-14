a17d367f59442869307bed24919b560aba4eeaa3
Akiyoshi Suda <aki.suda@protonmail.com>
2022-10-14 11:02:47 +0900

    i3status-rust: minimal

--- users/desktop/i3status-rust.nix
+++ users/desktop/i3status-rust.nix
@@ -4,6 +4,27 @@
 
     bars.default = {
       icons = "material-nf";
+      blocks = [
+        {
+          block = "sound";
+          on_click = "pavucontrol";
+        }
+        {
+          block = "networkmanager";
+          on_click = "nm-connection-editor";
+          ap_format = "{strength}{ssid}";
+          device_format = "{icon}{ap}";
+          connection_format = "";
+        }
+        {
+          block = "battery";
+          format = "{percentage}";
+        }
+        {
+          block = "time";
+          format = "%a %d/%m %R";
+        }
+      ];
     };
   };
 }
