e24a98242ccb26e5194723d351f0633e7f1149da
Akiyoshi Suda <aki.suda@protonmail.com>
2022-11-05 14:40:23 +0900

    change gtk theme

--- users/desktop/gtk.nix
+++ users/desktop/gtk.nix
@@ -1,5 +1,41 @@
 { lib, pkgs, ... }:
 
+let
+  rose-pine-gtk3 = with pkgs; stdenv.mkDerivation
+    rec {
+      pname = "rose-pine-gtk3";
+      version = "2.1.0";
+
+      src = fetchFromGitHub {
+        owner = "rose-pine";
+        repo = "gtk";
+        rev = "v${version}";
+        sha256 = "sha256-MT8AeC+uGRZS4zFNvAqxqSLVYpd9h64RdSvr6Ky4HA4=";
+      };
+
+      nativeBuildInputs = [
+        gtk3
+      ];
+
+      buildInputs = [
+        gnome-themes-extra
+      ];
+
+      propagatedUserEnvPkgs = [
+        gtk-engine-murrine
+      ];
+
+      installPhase = ''
+        runHook preInstall
+        mkdir -p $out/share/themes
+        cp -r gtk3/rose-pine-gtk $out/share/themes
+        cp -r gtk3/rose-pine-moon-gtk $out/share/themes
+        cp -r gtk3/rose-pine-dawn-gtk $out/share/themes
+        runHook postInstall
+      '';
+    };
+
+in
 {
   gtk = {
     enable = true;
@@ -11,8 +47,8 @@
     };
 
     theme = {
-      package = pkgs.adw-gtk3;
-      name = "adw-gtk3";
+      package = rose-pine-gtk3;
+      name = "rose-pine-dawn-gtk";
     };
 
     iconTheme = {
