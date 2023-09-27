ff07bd1759e2fabff99bff40e19f065b84f01527
Akiyoshi Suda <aki.suda@protonmail.com>
2023-09-27 22:21:21 +0900

    Add nix-community binary cache

--- home/default.nix
+++ home/default.nix
@@ -31,9 +31,16 @@ let
 
     nix.package = pkgs.nix;
     nix.settings.auto-optimise-store = true;
+    nix.settings.substituters = [
+      "https://cache.nixos.org/"
+      "https://nix-community.cachix.org"
+    ];
+    nix.settings.trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
     nix.registry.nixpkgs.flake = inputs.nixpkgs;
     nix.registry.nixpkgs-stable.flake = inputs.nixpkgs-stable;
-
     home.sessionVariables.NIX_PATH = lib.concatStringsSep ":" [
       "nixpkgs=flake:nixpkgs"
       "nixpkgs-stable=flake:nixpkgs-stable"
